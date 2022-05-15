1f9150938bbc0584d4d5414fa96c0fdb06c5e59d
pranavpatil2611 <patilpranav69.68@gmail.com>
2022-05-15 17:19:04 +0530

    secondcommit

--- Sum.java
+++ Sum.java
@@ -4,6 +4,7 @@ public static void main(String args[])
 {  
 int n1 = 10, n2 = 20, sum;  
 sum = n1 + n2;  
-System.out.println("The sum of numbers is: "+sum);  
+System.out.println("The sum of numbers is: "+sum);
+System.out.println("=====Thank You=====");
 }  
 }
